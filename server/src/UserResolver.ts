fee54547e1c886984f7b73996b364b84f0a4ef71
minadawood <mina.ziden@hotmail.com>
2021-07-23 03:53:16 +0200

    Access & Refresh Tokens

--- server/src/UserResolver.ts
+++ server/src/UserResolver.ts
@@ -1,6 +1,24 @@
-import { Arg, Mutation, Query, Resolver } from "type-graphql";
-import { hash } from "bcryptjs";
+import {
+  Arg,
+  Ctx,
+  Field,
+  Mutation,
+  ObjectType,
+  Query,
+  Resolver,
+  UseMiddleware,
+} from "type-graphql";
+import { compare, hash } from "bcryptjs";
 import { User } from "./entity/User";
+import { MyContext } from "./MyContext";
+import { createAccessToken, createRefreshToken } from "./auth";
+import { isAuth } from "./isAuth";
+
+@ObjectType()
+class LoginResponse {
+  @Field()
+  accessToken: string;
+}
 
 @Resolver()
 export class UserResolver {
@@ -9,9 +27,44 @@ export class UserResolver {
     return "Hello";
   }
 
+  @Query(() => String)
+  @UseMiddleware(isAuth)
+  bye(@Ctx() { payload }: MyContext) {
+    return `your user id is ${payload!.userId}`;
+  }
+
   @Query(() => [User])
   users() {
     return User.find();
+  }
+
+  @Mutation(() => LoginResponse)
+  async login(
+    @Arg("email") email: string,
+    @Arg("password") password: string,
+    @Ctx() { res }: MyContext
+  ): Promise<LoginResponse> {
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error("could not find user");
+    }
+
+    const valid = await compare(password, user.password);
+
+    if (!valid) {
+      throw new Error("bad password");
+    }
+
+    // Login successfull
+
+    res.cookie("jwt", createRefreshToken(user), {
+      httpOnly: true,
+    });
+
+    return {
+      accessToken: createAccessToken(user),
+    };
   }
 
   @Mutation(() => Boolean)
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,46 @@
+import "dotenv/config";
 import "reflect-metadata";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { UserResolver } from "./UserResolver";
 import { createConnection } from "typeorm";
+import cookieParser from "cookie-parser";
+import { verify } from "jsonwebtoken";
+import { User } from "./entity/User";
+import { createAccessToken } from "./auth";
 
 (async () => {
   const app = express();
+
+  app.use(cookieParser());
+
   app.get("/", (_, res) => {
     res.send("Hi");
+  });
+
+  app.post("/refresh_token", async (req, res) => {
+    const token = req.cookies.jwt;
+
+    if (!token) {
+      return res.send({ ok: false, accessToken: "" });
+    }
+
+    let payload: any = null;
+    try {
+      payload = verify(token, process.env.REFRESH_TOKEN_SECRET!);
+    } catch (err) {
+      console.log(err);
+      return res.send({ ok: false, accessToken: "" });
+    }
+
+    // Token is valid
+    const user = await User.findOne({ id: payload.userId });
+
+    if (!user) {
+      return res.send({ ok: false, accessToken: "" });
+    }
+    return res.send({ ok: true, accessToken: createAccessToken(user) });
   });
 
   await createConnection();
@@ -17,6 +49,7 @@ import { createConnection } from "typeorm";
     schema: await buildSchema({
       resolvers: [UserResolver],
     }),
+    context: ({ req, res }) => ({ req, res }),
   });
 
   apolloServer.applyMiddleware({ app });
